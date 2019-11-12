fbccfc673412861e135038478b300e05afc6dc63
r3dc0re <zxcvgod565@gmail.com>
2019-11-12 19:17:57 +0800

    Initial commit

---
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.test;
+
+import androidx.appcompat.app.ActionBar;
+import android.os.Bundle;
+
+import android.view.View;
+
+import android.widget.Button;
+
+import android.widget.TextView;
+import android.os.Bundle;
+
+import android.view.View;
+
+import android.widget.Button;
+
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.client_page);
+
+
+    }
+
+    public void login_in(View view){
+
+
+    }
+
+
+}
